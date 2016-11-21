c94183111e792df55d392974e9f0a3eb6b11ac2c
Cezzxd <cesar.moretti1@gmail.com>
2016-11-20 22:38:41 -0200

    Update index.php

--- Site/index.php
+++ Site/index.php
@@ -9,6 +9,11 @@
     <div class="geral">
         <?php include "directives/navView.php";?>
             <section class="center">
+                <form action="login.php" method="POST">
+                    <p>Usuário:<br/><input type="text" name="usuario" value="" id="usuario"/></p>
+                    <p>Senha:<br/><input type="password" name="senha" value="" id="senha"/></p>
+                    <p><input type="submit" value="LOGIN"/></p>
+                </form>
             </section>
         <?php include "directives/footerView.php";?>
         </div>
